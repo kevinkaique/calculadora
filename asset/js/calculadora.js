43b1c3efb5846e8c37cf1279ce65337a14ce865c
Kevin Kaique <kevinmatos.cedesp@gmail.com>
2024-10-31 18:48:42 -0300

    Add files via upload

---
+++ asset/js/calculadora.js
@@ -0,0 +1,41 @@
+//VARIAVEL GLOBAL
+const visor = document.getElementById("resultado");
+
+
+function insert(num) {
+    visor.textContent += num;
+}
+
+function clean() {
+    visor.textContent = "";
+}
+
+function backSpace() {
+    let numeroVisor = visor.textContent;
+
+    visor.textContent = numeroVisor.substring(0, numeroVisor.length -1)
+}
+
+function calcular() {
+    let calculo = visor.textContent;
+
+    if (calculo.length > 2) {
+    
+        visor.textContent = eval(calculo);
+
+    } else {
+        
+        visor.textContent = "Error!";
+        visor.style.color = '#f00';
+        visor.style.textAlign = 'center';
+
+        setTimeout(() => {
+            clean();
+            visor.style.color = '#000';
+            visor.style.textAlign = 'right';
+
+        }, 1000);
+    }
+
+    
+}
